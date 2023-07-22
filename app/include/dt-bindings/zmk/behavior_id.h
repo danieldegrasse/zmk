0a8a372378e8351e8f00cdc9aef1a2be26f8323d
Daniel DeGrasse <daniel@degrasse.com>
2023-07-21 19:39:32 -0500

    [noup] feat: added behavior-id field to all in tree behaviors
    Added behavior-id field to all in tree behaviors. This will allow the
    behavior to be referenced by a fixed length identifier
    
    Signed-off-by: Daniel DeGrasse <daniel@degrasse.com>

---
+++ app/include/dt-bindings/zmk/behavior_id.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2023 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#define BEHAVIOR_BACKLIGHT                   0
+#define BEHAVIOR_BLUETOOTH                   1
+#define BEHAVIOR_CAPS_WORD                   2
+#define BEHAVIOR_EXT_POWER                   3
+#define BEHAVIOR_GRESC                       4
+#define BEHAVIOR_KEY_PRESS                   5
+#define BEHAVIOR_KEY_REPEAT                  6
+#define BEHAVIOR_KEY_TOGGLE                  7
+#define BEHAVIOR_LAYER_TAP                   8
+#define BEHAVIOR_MACRO_TAP                   9
+#define BEHAVIOR_MACRO_PRESS                 10
+#define BEHAVIOR_MACRO_RELEASE               11
+#define BEHAVIOR_MACRO_TAP_TIME              12
+#define BEHAVIOR_MACRO_WAIT_TIME             13
+#define BEHAVIOR_MACRO_PAUSE_FOR_RELEASE     14
+#define BEHAVIOR_MOD_TAP                     15
+#define BEHAVIOR_MOMENTARY_LAYER             16
+#define BEHAVIOR_NONE                        17
+#define BEHAVIOR_OUTPUTS                     18
+#define BEHAVIOR_RESET                       19
+#define BEHAVIOR_BOOTLOAD                    20
+#define BEHAVIOR_RGB_UNDERGLOW               21
+#define BEHAVIOR_SENSOR_ROTATE_KEY_PRESS     22
+#define BEHAVIOR_STICKY_KEY                  23
+#define BEHAVIOR_STICKY_LAYER                24
+#define BEHAVIOR_TO_LAYER                    25
+#define BEHAVIOR_TOGGLE_LAYER                26
+#define BEHAVIOR_TRANSPARENT                 27
+#define BEHAVIOR_MACRO_PARAM_1TO1            28
+#define BEHAVIOR_MACRO_PARAM_1TO2            29
+#define BEHAVIOR_MACRO_PARAM_2TO1            30
+#define BEHAVIOR_MACRO_PARAM_2TO2            31
+#define BEHAVIOR_ANIMATION                   32
